b92de5c0777da1d18defec5236f8148c2c1c6952
HtetOoNaing <htetoonaing.ycc@gmail.com>
2020-12-06 17:00:51 +0630

    create react scheduler

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import { Inject, ScheduleComponent, Day, Week, WorkWeek, Month, Agenda } from '@syncfusion/ej2-react-schedule'
+function App() {
+  return (
+    <ScheduleComponent>
+      <Inject services={[Day, Week, WorkWeek, Month, Agenda]} />
+    </ScheduleComponent>
+  );
+}
+
+export default App;
